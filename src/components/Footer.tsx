8cdda438fb98641c09253e56f13dc53bbfa7e709
kpiyush25 <piyushkumar.94528@gmail.com>
2025-07-09 19:20:21 +0530

    Keep Delhi as headquarter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,7 +100,7 @@ const Footer: React.FC = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin size={16} className="text-primary-400" />
-                <span>Bengaluru, Karnataka, India</span>
+                <span>Delhi, India</span>
               </div>
             </div>
           </div>
